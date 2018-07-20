84346cbc7c08c77fd5365b10b0df88cd3645b1ee
Luke Vandertie <36864496+lvandertie@users.noreply.github.com>
2018-07-20 11:38:51 -0700

    Set up framework for logistic regression
    setwd
    read.csv
    convert categorical var (and any other modifications)
    Run glm

--- McKinsey_July_2018.r
+++ McKinsey_July_2018.r
@@ -2,6 +2,15 @@
 # McKinsey Hackathon July 20-22, 2018
 
 #----Logistic Regression to create benchmark probability model----
+setwd("C:/Users/lvandertie001/Documents/GitHub/McKinsey-Hackathon-07-18")
+# Read data
+train.data <- read.csv(file = "train_ZoGVYWq.csv")
+
+# Convert categorical variables
+
+# Potentially combine the count of months into one ordinal variable
+
+# Run glm
 
 
 #----Plug Premium into optimization equation to get ideal Incentive
@@ -15,3 +24,4 @@ Premium <- 3000
 incentive <- -400 * log( log(200/2.706706 * (100/Premium))/3  ) 
 
 curve(log(log(200/2.706706 * (100/x))/3), from = 0, to = 10000, xlab = "Premium", ylab = "Incentive")
+
